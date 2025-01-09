69880ac3c47316c847402d1c4dea1d4e370a914e
Jeff Campbell <jeffcampbell333@gmail.com>
2025-01-09 15:52:02 -0800

    added react-icons and avatar icon

--- src/app/components/header/login-avatar.tsx
+++ src/app/components/header/login-avatar.tsx
@@ -1,9 +1,10 @@
-
+import { RxAvatar } from "react-icons/rx";
 
 export default function LoginAvatar(){
   return(
     <div>
       {/*User Avatar allows user to login/register/logout */}
+      <RxAvatar />
     </div>
   )
 }
