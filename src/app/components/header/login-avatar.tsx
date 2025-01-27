7cf0efce1cc43959c81c538061e3baf507358e44
Jeff Campbell <jeffcampbell333@gmail.com>
2025-01-26 19:18:32 -0800

    added functionality to avatar button

--- src/app/components/header/login-avatar.tsx
+++ src/app/components/header/login-avatar.tsx
@@ -3,19 +3,22 @@
 import { useState } from "react";
 
 import { RxAvatar } from "react-icons/rx";
+import { FaBookReader } from "react-icons/fa";
 
 export default function LoginAvatar(){
 
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   const onAvatarClick = () => {
-
+    setIsLoggedIn(!isLoggedIn);
   }
 
   return(
     <div>
       {/*User Avatar allows user to login/register/logout */}
-      <button>
-        <RxAvatar />
+      <button
+        onClick={()=>onAvatarClick()}
+      >
+        {isLoggedIn ? <FaBookReader/> : <RxAvatar/>}
       </button>
     </div>
   )
