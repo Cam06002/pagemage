53b49e8381d0cb2979b7a06a7e0d02d70524d44f
Jeff Campbell <jeffcampbell333@gmail.com>
2025-03-20 19:32:36 -0700

    added and styled search bar

--- src/app/components/search-bar/search-bar.tsx
+++ src/app/components/search-bar/search-bar.tsx
@@ -1,10 +1,13 @@
-
+import { IoSearchSharp } from "react-icons/io5";
 
 export default function SearchBar(){
   return(
     <div>
       {/*Search bar takes user input, calls GoogleAPI and navigates users to results page*/}
-
+      <form className="flex">
+        <input type="text" placeholder="search" className="w-xl h-xs flex text-black p-1 rounded-md text-sm"></input>
+        <button type="submit" className="text-lg ml-1"><IoSearchSharp/></button>
+      </form>
     </div>
   )
 }
