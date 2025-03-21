54a899eddac1b7e17c0e35c348a305f8a53b7a9c
Jeff Campbell <jeffcampbell333@gmail.com>
2025-03-20 20:07:08 -0700

    added page image and title text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Reader from "../../public/reading.svg";
 export default function Home() {
   return (
     <main>
-      <div className="h-20 w-auto">
+      <div className="flex justify-center flex-col text-center mt-12">
         <Image
           src={Reader}
           alt="A woman reading a book"
+          className="h-80 w-auto invert scale-x-[-1]"
         />
+        <h1 className="text-5xl">There's Magic to Discover</h1>
       </div>
     </main>
   );
