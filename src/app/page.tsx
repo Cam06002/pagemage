a4d0df36d3bad631a631198aded73dae0094be0c
Jeff Campbell <jeffcampbell333@gmail.com>
2025-03-20 19:48:41 -0700

    added reader image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import Reader from "../../public/reading.svg";
 
 export default function Home() {
   return (
     <main>
+      <div className="h-20 w-auto">
+        <Image
+          src={Reader}
+          alt="A woman reading a book"
+        />
+      </div>
     </main>
   );
 }
