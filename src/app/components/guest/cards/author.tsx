770f71dd8dc49b1f057f20cab05f93705d4ba808
Jeff Campbell <jeffcampbell333@gmail.com>
2025-04-02 21:03:34 -0700

    styling of authors card

--- src/app/components/guest/cards/author.tsx
+++ src/app/components/guest/cards/author.tsx
@@ -3,15 +3,17 @@ import { TiStar, TiHeart } from "react-icons/ti";
 
 export default function Author({name, image, likes, favorites}:{name:string, image:any, likes:string, favorites:string}){
   return(
-    <div className="flex flex-col justify-between align-middle h-[392px] w-[200px] mx-2 border-2 rounded-xl p-2">
+    <div className="flex flex-col justify-between align-middle h-[392px] w-[250px] mx-2 border-2 rounded-xl p-2">
       <h4 className="text-xl">{name}</h4>
-      <Image
-        src={image}
-        height={302}
-        width={200}
-        alt="book cover image"
-        className="rounded object-fill"
-      />
+      <div className="flex flex-row justify-center">
+        <Image
+          src={image}
+          height={302}
+          width={200}
+          alt="book cover image"
+          className="rounded object-fill"
+        />
+      </div>
       <div className="flex flex-row justify-center">
         <TiStar /><p className="ml-1 mr-4">{likes}</p>
         <TiHeart /><p className="ml-1">{favorites}</p>
